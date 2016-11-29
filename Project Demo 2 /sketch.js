204de7a3ab8f309b10b1e315d8515f43953ecd3a
ordinarygraphix <stephaniecenfung@gmail.com>
2016-11-29 13:32:36 -0500

    project demo 2

---
+++ Project Demo 2 /sketch.js
@@ -0,0 +1,60 @@
+var video1, video2, video3;
+var mouse_clicked = false;
+var video1_playing = false;
+var video2_playing = false;
+var video3_playing = false;
+
+//hide video and create image onto canvas 
+
+function setup() {
+  //noCanvas();
+  //createCanvas(1000,800);
+  //volume(0.0);
+  canvas = createCanvas(1000, 800);
+
+  video1 = createVideo("nicole.MOV");
+  video1.size(1000, 800);
+  video1.loop();
+  video1.hide();
+
+  //video2 = createVideo("mags.MOV");
+  //video2.size(1000,800);
+  //video3 = createVideo("lily.MOV");
+  //video3.size(1000,800);
+  //video1.loop();  
+}
+
+function draw() {
+  background(0);
+  image(video1, 0, 0);
+  fill(244, 55, 0);
+  rect(500, 300, 100, 100);
+}
+
+/*function mousePressed() {
+  if (mouse_clicked == false) {
+    video1.play();
+    mouse_clicked = true;
+    video1_playing = true;
+  } else if (mouse_clicked && video1_playing) {
+    video1.pause();
+    video1.hide();
+    video2 = createVideo("mags.MOV");
+    video2.size(1000, 800);
+    video2.play();
+    mouse_clicked = false;
+    video1_playing = false;
+    video2_playing = true;
+    mouse_clicked = true;
+  } else if (mouse_clicked && video2_playing) {
+    video2.pause();
+    video2.hide();
+    video3 = createVideo("lily.MOV");
+    video3.size(1000, 800);
+    video3.play();
+    mouse_clicked = false;
+    video2_playing = false;
+    video3_playing = true;
+  }
+
+}*/
